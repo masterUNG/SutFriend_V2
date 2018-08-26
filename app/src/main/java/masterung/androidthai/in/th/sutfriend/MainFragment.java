f981cd38746230dd7a954d66b16397fe439d41cd
masterUNG <phrombutr@gmail.com>
2018-08-26 15:36:24 +0700

    update6

--- app/src/main/java/masterung/androidthai/in/th/sutfriend/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/MainFragment.java
@@ -8,8 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainFragment extends Fragment{
@@ -24,7 +29,54 @@ public class MainFragment extends Fragment{
 //        Check Login
         checkLogin();
 
+//        Login Controller
+        loginController();
+
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+                final MyAlert myAlert = new MyAlert(getActivity());
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    myAlert.normalDialog("Have Space",
+                            "Please Fill All Blank");
+                } else {
+
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        moveToService();
+                                    } else {
+                                        myAlert.normalDialog("Login False",
+                                                task.getException().getMessage().toString());
+                                    }
+
+                                }
+                            });
+
+
+
+                }   // if
+
+
+
+            }
+        });
+    }
 
     private void checkLogin() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
